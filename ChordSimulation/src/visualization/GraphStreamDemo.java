bd21c337a9fd4b6c36c6e6d1e6ebbc9af8737042
Zhengyang Tang <ztang@terpmail.umd.edu>
2019-10-17 19:26:19 -0400

    Adding GraphStream dependency to maven

---
+++ ChordSimulation/src/visualization/GraphStreamDemo.java
@@ -0,0 +1,21 @@
+package visualization;
+
+import org.graphstream.graph.*;
+import org.graphstream.graph.implementations.*;
+
+public class GraphStreamDemo {
+
+	public static void main(String[] args) {
+		
+		Graph graph = new SingleGraph("Tutorial 1");
+		graph.addNode("A" );
+		graph.addNode("B" );
+		graph.addNode("C" );
+		graph.addEdge("AB", "A", "B");
+		graph.addEdge("BC", "B", "C");
+		graph.addEdge("CA", "C", "A");
+		
+		graph.display();
+	}
+	
+}
